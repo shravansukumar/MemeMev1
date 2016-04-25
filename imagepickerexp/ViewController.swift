6e87b06f0f7f4f2af5978b05b9b2954ae16c1fc6
shravansukumar <shravan.sukumar@gmail.com>
2016-04-25 20:07:17 +0530

    initial commit

---
+++ imagepickerexp/ViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  imagepickerexp
+//
+//  Created by Shravan Sukumar on 15/04/16.
+//  Copyright © 2016 shravan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate{
+
+    @IBOutlet var imagePickerView: UIImageView!
+    @IBOutlet var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet var topTextField: UITextField!
+    @IBOutlet var bottomTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        
+        
+        topTextField.defaultTextAttributes = memeTextAtributes
+        bottomTextField.defaultTextAttributes = memeTextAtributes
+        
+        topTextField.textAlignment = NSTextAlignment.Center
+        bottomTextField.textAlignment = NSTextAlignment.Center
+        
+        textFieldDidBeginEditing(topTextField)
+        textFieldDidBeginEditing(bottomTextField)
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
+        self.subscribeToKeyboardNotificatons()
+        
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unsubscribeToKeyboardNotifications()
+    }
+    
+    
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func subscribeToKeyboardNotificatons() {
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification){
+        if bottomTextField.isFirstResponder() {
+        self.view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    func keyboardWillHide(notification : NSNotification ) {
+        if bottomTextField.isFirstResponder() {
+        self.view.frame.origin.y = 0
+        }
+    }
+    
+   
+    
+    let memeTextAtributes = [NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName :UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 35)!,
+        NSStrokeWidthAttributeName : "6.0"
+        
+    ]
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if topTextField.text == "TOP" && bottomTextField.text == "BOTTOM" {
+            textField.clearsOnBeginEditing = true
+        }
+        else {
+            textField.clearsOnBeginEditing = false
+  
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        topTextField.resignFirstResponder()
+        bottomTextField.resignFirstResponder()
+        return true
+    }
+
+    
+    func imagePickerController(picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            
+            
+        }
+    }
+    
+    func  imagePickerControllerDidCancel(picker: UIImagePickerController){
+        dismissViewControllerAnimated(true, completion: nil)
+
+    }
+    
+    @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        
+        
+    }
+   
+    @IBAction func pickAnImageFromCamera(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+
+}
+
